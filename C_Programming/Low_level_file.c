b03d8b63f8116d9fe59074486116fb44b54e0c7d
Bhargavi-Uthayakumar <thisisbhargavi1999@gmail.com>
2025-09-03 10:17:38 +0530

    Add files via upload

---
+++ C_Programming/Low_level_file.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 4096
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        printf("Usage: %s <source_file> <dest_file>\n", argv[0]);
+        exit(0);
+    }
+
+
+int src_fd,dest_fd;
+ssize_t nread,nwrite;
+char buffer[BUFFER_SIZE];
+
+src_fd = open(argv[1],O_RDONLY);
+if(src_fd < 0)
+{
+    perror("open");
+    return 1;
+}
+
+dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+if(dest_fd < 0)
+{
+    perror("open");
+    return 1;
+}
+
+while((nread = read(src_fd,buffer,BUFFER_SIZE)) > 0)
+{
+    nwrite = write(dest_fd,buffer,nread);
+    if(nwrite<0)
+    {
+        perror("write");
+        close(dest_fd);
+        return 1;
+    }
+    if(nread!=nwrite)
+    {
+        perror("write");
+        close(src_fd);
+        close(dest_fd);
+        return 1;
+    }
+}
+
+if(nread < 0)
+{
+    perror("read");
+    close(src_fd);
+    return 1;
+}
+
+char new_character = 'X';
+if(lseek(dest_fd,6,SEEK_SET) == (off_t)-1)
+{
+    perror("lseek");
+    return -1;
+}
+else
+{
+    if(write(dest_fd,"X",1)!=1)
+    {
+        perror("overwrite");
+        close(dest_fd);
+    }
+}
+
+close(src_fd);
+close(dest_fd);
+
+return 0;
+}
