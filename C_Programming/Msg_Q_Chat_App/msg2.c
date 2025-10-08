2befb33542378dfcc6e79a4a6363cb79312e3eed
Bhargavi-Uthayakumar <thisisbhargavi1999@gmail.com>
2025-10-08 10:39:46 +0530

    Add files via upload

---
+++ C_Programming/Msg_Q_Chat_App/msg2.c
@@ -0,0 +1,53 @@
+#include "header.h"
+
+int main()
+{
+    key_t key = ftok("chat.h", 65); 
+    if (key == -1)
+    {
+        die("ftok");
+    }
+
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+    if (msgid == -1)
+    {
+        die("msgget");
+    }
+
+    Message msg;
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        while (1)
+        {
+            if (msgrcv(msgid, &msg, sizeof(msg.mtext), MSG_USER1_TO_USER2, 0) == -1)
+            {
+                die("msgrcv");
+            }
+            printf("[User1]: %s\n", msg.mtext);
+        }
+    }
+    else
+    {
+        while (1)
+        {
+            printf("[User2]: ");
+            fflush(stdout);
+            if (!fgets(msg.mtext, MAX_TEXT, stdin))
+            {
+                break;
+            }
+
+            msg.mtext[strcspn(msg.mtext, "\n")] = '\0'; 
+            msg.mtype = MSG_USER2_TO_USER1;
+
+            if (msgsnd(msgid, &msg, strlen(msg.mtext) + 1, 0) == -1)
+            {
+                die("msgsnd");
+            }
+        }
+    }
+
+    return 0;
+}
